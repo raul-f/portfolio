10939c4b33d69a51b63c83c962f451f095e9fd87
raul-f <raulf.dev@gmail.com>
2018-12-31 20:00:06 -0300

    Hosted background images locally & other changes

--- app.js
+++ app.js
@@ -6,12 +6,12 @@ const app = express();
 app.use('/public', express.static(path.join(__dirname, 'static')));
 
 app.get('/', (req, res) => {
-    console.log(`${req.method} request to ${req.url} has been received`);
+    //console.log(`${req.method} request to ${req.url} has been received`);
     res.sendFile(path.join(__dirname, 'main/index.html'));
 });
 
 app.get('/project-collection', (req, res) => {
-    console.log(`${req.method} request to ${req.url} has been received`);
+    //console.log(`${req.method} request to ${req.url} has been received`);
     res.sendFile(path.join(__dirname, 'project-collection/index.html'));
 });
 
