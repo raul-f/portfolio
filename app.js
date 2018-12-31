759558a19f0aeb636e971abc42ce0dc116b4c447
raul-f <raulf.dev@gmail.com>
2018-12-31 14:10:53 -0300

    Early working build

--- app.js
+++ app.js
@@ -3,10 +3,10 @@ const path = require('path');
 
 const app = express();
 
-app.use('/public', express.static(path.join(__dirname + 'main')));
+app.use('/public', express.static(path.join(__dirname, 'main')));
 
 app.get('/', (req, res) => {
-    res.sendFile('./main/index.html');
+    res.sendFile(path.join(__dirname, 'main/index.html'));
 });
 
 app.listen(3000);
