65b6cda19bdc7dcc7a4fc69798c8ac744c68c8fa
raul-f <raulf.dev@gmail.com>
2019-01-18 14:53:00 -0200

    improved Adresses.js structure

--- app/App.js
+++ app/App.js
@@ -2,7 +2,6 @@ import React from "react"
 import { BrowserRouter, Route } from "react-router-dom"
 
 import Home from "./components/Home.js"
-import Navbar from "./components/home/Navbar"
 import About from "./components/About.js"
 import Projects from "./components/Projects.js"
 import Contact from "./components/Contact.js"
--- app/components/contact/Adresses.js
+++ app/components/contact/Adresses.js
@@ -4,16 +4,22 @@ const Adresses = () => {
     return (
         <ul className="addresses">
             <li className="address-item">
-                <i className="fas fa-envelope address-icon" />
-                <p className="address-text">raulf.dev@gmail.com</p>
+                <p className="address-text">
+                    <i className="fas fa-envelope address-icon" />
+                    raulf.dev@gmail.com
+                </p>
             </li>
             <li className="address-item">
-                <i className="fas fa-phone fa-flip-horizontal address-icon" />
-                <p className="address-text">+55 (11) 98104-1711</p>
+                <p className="address-text">
+                    <i className="fas fa-phone fa-flip-horizontal address-icon" />
+                    +55 (11) 98104-1711
+                </p>
             </li>
             <li className="address-item">
-                <i className="fab fa-twitter address-icon" />
-                <p className="address-text">@raulf_dev</p>
+                <p className="address-text">
+                    <i className="fab fa-twitter address-icon" />
+                    @raulf_dev
+                </p>
             </li>
         </ul>
     )
