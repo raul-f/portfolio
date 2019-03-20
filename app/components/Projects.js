8c26f66b5d8c5c63604444ea6819c6c3dd121555
raul-f <raulf.dev@gmail.com>
2019-03-20 15:53:47 -0300

    changed wheel event to active

--- app/components/Projects.js
+++ app/components/Projects.js
@@ -1,300 +1,289 @@
-import React from "react"
-import anime from "animejs"
+import React from 'react'
+import anime from 'animejs'
 
-import Navbar from "./Navbar"
-import LinkBox from "./LinkBox"
-import Logo from "./Logo"
-import Filter from "./projects/Filter"
-import Display from "./projects/Display"
-import ScrollBtn from "./projects/ScrollBtn"
-import Watermark from "./Watermark"
-import MobileMenu from "./MobileMenu"
+import Navbar from './Navbar'
+import LinkBox from './LinkBox'
+import Logo from './Logo'
+import Filter from './projects/Filter'
+import Display from './projects/Display'
+import ScrollBtn from './projects/ScrollBtn'
+import Watermark from './Watermark'
+import MobileMenu from './MobileMenu'
 
 class Projects extends React.Component {
-    constructor(props) {
-        super(props)
-        this.state = {
-            selectedSet: "personal-picks",
-            activeSet: "personal-picks",
-            selectedProject: "shortr",
-            activeProject: "shortr",
-            settings: {
-                targets: ".scroll-btn",
-                easing: "easeInOutCubic",
-                duration: 450
-            },
-            rotate: 0,
-            style: { transform: "rotate(0deg)" },
-            animating: false,
-            start: null
-        }
+  constructor(props) {
+    super(props)
+    this.state = {
+      selectedSet: 'personal-picks',
+      activeSet: 'personal-picks',
+      selectedProject: 'shortr',
+      activeProject: 'shortr',
+      settings: {
+        targets: '.scroll-btn',
+        easing: 'easeInOutCubic',
+        duration: 450,
+      },
+      rotate: 0,
+      style: { transform: 'rotate(0deg)' },
+      animating: false,
+      start: null,
     }
+  }
 
-    handleFilterSelection = event => {
-        if (window.innerWidth < 500 && window.innerHeight < 800) {
-            document.documentElement.requestFullscreen()
-            window.scrollTo({
-                top: 2 * window.innerHeight,
-                behavior: "smooth"
-            })
-            this.setState(
-                {
-                    rotate: this.state.rotate + 180,
-                    animating: true
-                },
-                () => {
-                    anime({
-                        ...this.state.settings,
-                        rotate: this.state.rotate
-                    }).finished.then(() => {
-                        this.setState({ animating: false })
-                    })
-                }
-            )
-        }
-        const initProjects = {
-            "personal-picks": "shortr",
-            "html-css-websites": "tribute-page",
-            "single-page-apps": "random-quote-machine",
-            "d3js-charts": "bar-chart",
-            "full-stack-web-apps": "shortr",
-            microservices: "timely"
-        }
-        this.setState({
-            selectedSet: event.target.id,
-            activeSet: event.target.id,
-            selectedProject: initProjects[event.target.id],
-            activeProject: initProjects[event.target.id]
+  handleFilterSelection = event => {
+    if (window.innerWidth < 500 && window.innerHeight < 800) {
+      document.documentElement.requestFullscreen()
+    }
+    window.scrollTo({
+      top: 2 * window.innerHeight,
+      behavior: 'smooth',
+    })
+    this.setState(
+      {
+        rotate: this.state.rotate + 180,
+        animating: true,
+      },
+      () => {
+        anime({
+          ...this.state.settings,
+          rotate: this.state.rotate,
+        }).finished.then(() => {
+          this.setState({ animating: false })
         })
+      }
+    )
+    const initProjects = {
+      'personal-picks': 'shortr',
+      'html-css-websites': 'tribute-page',
+      'single-page-apps': 'random-quote-machine',
+      'd3js-charts': 'bar-chart',
+      'full-stack-web-apps': 'shortr',
+      microservices: 'timely',
     }
+    this.setState({
+      selectedSet: event.target.id,
+      activeSet: event.target.id,
+      selectedProject: initProjects[event.target.id],
+      activeProject: initProjects[event.target.id],
+    })
+  }
 
-    handleFilterPreview = event => {
-        switch (event.type) {
-            case "mouseover":
-                this.setState({ activeSet: event.target.id })
-                break
-            default:
-                this.setState({ activeSet: this.state.selectedSet })
-                break
-        }
+  handleFilterPreview = event => {
+    switch (event.type) {
+      case 'mouseover':
+        this.setState({ activeSet: event.target.id })
+        break
+      default:
+        this.setState({ activeSet: this.state.selectedSet })
+        break
     }
+  }
 
-    handleDisplaySelection = event => {
-        if (window.innerWidth < 500) {
-            document.documentElement.requestFullscreen()
-        }
-        this.setState({
-            selectedProject: event.target.id,
-            activeProject: event.target.id
+  handleDisplaySelection = event => {
+    if (window.innerWidth < 500) {
+      document.documentElement.requestFullscreen()
+    }
+    this.setState({
+      selectedProject: event.target.id,
+      activeProject: event.target.id,
+    })
+  }
+
+  handleDisplayPreview = event => {
+    switch (event.type) {
+      case 'mouseover':
+        this.setState({ activeProject: event.target.id })
+        break
+      default:
+        this.setState({ activeProject: this.state.selectedProject })
+        break
+    }
+  }
+
+  handleScrollBtn = () => {
+    if (window.innerWidth < 500) {
+      document.documentElement.requestFullscreen()
+    }
+    if (!this.state.animating) {
+      if ((this.state.rotate / 180) % 2 === 1) {
+        window.scrollTo({ top: 0, behavior: 'smooth' })
+      } else {
+        window.scrollTo({
+          top: 2 * window.innerHeight,
+          behavior: 'smooth',
         })
-    }
-
-    handleDisplayPreview = event => {
-        switch (event.type) {
-            case "mouseover":
-                this.setState({ activeProject: event.target.id })
-                break
-            default:
-                this.setState({ activeProject: this.state.selectedProject })
-                break
+      }
+      this.setState(
+        {
+          rotate: this.state.rotate + 180,
+          animating: true,
+        },
+        () => {
+          anime({
+            ...this.state.settings,
+            rotate: this.state.rotate,
+          }).finished.then(() => {
+            this.setState({ animating: false })
+          })
         }
+      )
     }
+  }
 
-    handleScrollBtn = () => {
-        if (window.innerWidth < 500) {
-            document.documentElement.requestFullscreen()
+  handleScrolling = event => {
+    event.preventDefault()
+    if (event.deltaY > 0 && window.pageYOffset < window.innerHeight && !this.state.animating) {
+      window.scrollTo({ top: 2 * window.innerHeight, behavior: 'smooth' })
+      this.setState(
+        {
+          rotate: this.state.rotate + 180,
+          animating: true,
+        },
+        () => {
+          anime({
+            ...this.state.settings,
+            rotate: this.state.rotate,
+          }).finished.then(() => this.setState({ animating: false }))
         }
-        if (!this.state.animating) {
-            if ((this.state.rotate / 180) % 2 === 1) {
-                window.scrollTo({ top: 0, behavior: "smooth" })
-            } else {
-                window.scrollTo({
-                    top: 2 * window.innerHeight,
-                    behavior: "smooth"
-                })
-            }
-            this.setState(
-                {
-                    rotate: this.state.rotate + 180,
-                    animating: true
-                },
-                () => {
-                    anime({
-                        ...this.state.settings,
-                        rotate: this.state.rotate
-                    }).finished.then(() => {
-                        this.setState({ animating: false })
-                    })
-                }
-            )
+      )
+    } else if (
+      window.pageYOffset >= window.innerHeight &&
+      event.deltaY < 0 &&
+      !this.state.animating
+    ) {
+      window.scrollTo({
+        top: 0,
+        behavior: 'smooth',
+      })
+      this.setState(
+        {
+          rotate: this.state.rotate + 180,
+          animating: true,
+        },
+        () => {
+          anime({
+            ...this.state.settings,
+            rotate: this.state.rotate,
+          }).finished.then(() => this.setState({ animating: false }))
         }
+      )
     }
+  }
 
-    handleScrolling = event => {
+  handleTouch = event => {
+    switch (event.type) {
+      case 'touchstart':
+        this.setState({ start: event.touches[0].clientY })
+        break
+      case 'touchmove':
         event.preventDefault()
+        let current = event.touches[0]
         if (
-            event.deltaY > 0 &&
-            window.pageYOffset < window.innerHeight &&
-            !this.state.animating
+          current.clientY < this.state.start - 5 &&
+          !this.state.animating &&
+          window.pageYOffset < window.innerHeight
         ) {
-            window.scrollTo({ top: 2 * window.innerHeight, behavior: "smooth" })
-            this.setState(
-                {
-                    rotate: this.state.rotate + 180,
-                    animating: true
-                },
-                () => {
-                    anime({
-                        ...this.state.settings,
-                        rotate: this.state.rotate
-                    }).finished.then(() => this.setState({ animating: false }))
-                }
-            )
+          window.scrollTo({
+            top: 2 * window.innerHeight,
+            behavior: 'smooth',
+          })
+          this.setState(
+            {
+              rotate: this.state.rotate + 180,
+              animating: true,
+            },
+            () => {
+              anime({
+                ...this.state.settings,
+                rotate: this.state.rotate,
+              }).finished.then(() => this.setState({ animating: false }))
+            }
+          )
         } else if (
-            window.pageYOffset >= window.innerHeight &&
-            event.deltaY < 0 &&
-            !this.state.animating
+          current.clientY > this.state.start + 5 &&
+          !this.state.animating &&
+          window.pageYOffset >= window.innerHeight
         ) {
-            window.scrollTo({
-                top: 0,
-                behavior: "smooth"
-            })
-            this.setState(
-                {
-                    rotate: this.state.rotate + 180,
-                    animating: true
-                },
-                () => {
-                    anime({
-                        ...this.state.settings,
-                        rotate: this.state.rotate
-                    }).finished.then(() => this.setState({ animating: false }))
-                }
-            )
+          window.scrollTo({
+            top: 0,
+            behavior: 'smooth',
+          })
+          this.setState(
+            {
+              rotate: this.state.rotate + 180,
+              animating: true,
+            },
+            () => {
+              anime({
+                ...this.state.settings,
+                rotate: this.state.rotate,
+              }).finished.then(() => this.setState({ animating: false }))
+            }
+          )
         }
     }
+  }
 
-    handleTouch = event => {
-        switch (event.type) {
-            case "touchstart":
-                this.setState({ start: event.touches[0].clientY })
-                break
-            case "touchmove":
-                event.preventDefault()
-                let current = event.touches[0]
-                if (
-                    current.clientY < this.state.start - 5 &&
-                    !this.state.animating &&
-                    window.pageYOffset < window.innerHeight
-                ) {
-                    window.scrollTo({
-                        top: 2 * window.innerHeight,
-                        behavior: "smooth"
-                    })
-                    this.setState(
-                        {
-                            rotate: this.state.rotate + 180,
-                            animating: true
-                        },
-                        () => {
-                            anime({
-                                ...this.state.settings,
-                                rotate: this.state.rotate
-                            }).finished.then(() =>
-                                this.setState({ animating: false })
-                            )
-                        }
-                    )
-                } else if (
-                    current.clientY > this.state.start + 5 &&
-                    !this.state.animating &&
-                    window.pageYOffset >= window.innerHeight
-                ) {
-                    window.scrollTo({
-                        top: 0,
-                        behavior: "smooth"
-                    })
-                    this.setState(
-                        {
-                            rotate: this.state.rotate + 180,
-                            animating: true
-                        },
-                        () => {
-                            anime({
-                                ...this.state.settings,
-                                rotate: this.state.rotate
-                            }).finished.then(() =>
-                                this.setState({ animating: false })
-                            )
-                        }
-                    )
-                }
-        }
+  componentDidMount() {
+    document.addEventListener('wheel', this.handleScrolling)
+    document.addEventListener('touchmove', this.handleTouch, {
+      passive: false,
+    })
+    document.addEventListener('touchstart', this.handleTouch, {
+      passive: false,
+    })
+    if (window.pageYOffset < 100) {
+      window.scrollTo({ top: 0, behavior: 'auto' })
+    } else {
+      window.scrollTo({ top: window.innerHeight, behavior: 'auto' })
+      this.setState({
+        rotate: this.state.rotate + 180,
+        style: { transform: 'rotate(180deg)' },
+      })
     }
+  }
 
-    componentDidMount() {
-        document.addEventListener("wheel", this.handleScrolling)
-        document.addEventListener("touchmove", this.handleTouch, {
-            passive: false
-        })
-        document.addEventListener("touchstart", this.handleTouch, {
-            passive: false
-        })
-        if (window.pageYOffset < 100) {
-            window.scrollTo({ top: 0, behavior: "auto" })
-        } else {
-            window.scrollTo({ top: window.innerHeight, behavior: "auto" })
-            this.setState({
-                rotate: this.state.rotate + 180,
-                style: { transform: "rotate(180deg)" }
-            })
-        }
-    }
+  componentWillUnmount() {
+    document.removeEventListener('wheel', this.handleScrolling, { passive: false })
+    document.removeEventListener('touchmove', this.handleTouch, { passive: false })
+    document.removeEventListener('touchstart', this.handleTouch, { passive: false })
+  }
 
-    componentWillUnmount() {
-        document.removeEventListener("wheel", this.handleScrolling)
-        document.removeEventListener("touchmove", this.handleTouch)
-        document.removeEventListener("touchstart", this.handleTouch)
-    }
-
-    render() {
-        return (
-            <div id="projects" className="projects">
-                <img
-                    src="https://s3-sa-east-1.amazonaws.com/myhostedfiles.raulf/Images/portfolio-assets/projects-background.jpg"
-                    className="mobile-background"
-                />
-                <div className="projects-background" />
-                <MobileMenu language={this.props.language} />
-                <Logo />
-                <Navbar language={this.props.language} />
-                <div className="content">
-                    <Filter
-                        selected={this.state.selectedSet}
-                        active={this.state.activeSet}
-                        changeFilter={this.handleFilterSelection}
-                        previewFilter={this.handleFilterPreview}
-                        language={this.props.language}
-                    />
-                    <Display
-                        set={this.state.selectedSet}
-                        selected={this.state.selectedProject}
-                        active={this.state.activeProject}
-                        changeProject={this.handleDisplaySelection}
-                        previewProject={this.handleDisplayPreview}
-                        language={this.props.language}
-                    />
-                </div>
-                <ScrollBtn
-                    update={this.handleScrollBtn}
-                    style={this.state.style}
-                />
-                <Watermark language={this.props.language} page="projects" />
-                <LinkBox />
-                <div className="mobile-footer" />
-            </div>
-        )
-    }
+  render() {
+    return (
+      <div id="projects" className="projects">
+        <img
+          src="https://s3-sa-east-1.amazonaws.com/myhostedfiles.raulf/Images/portfolio-assets/projects-background.jpg"
+          className="mobile-background"
+        />
+        <div className="projects-background" />
+        <MobileMenu language={this.props.language} />
+        <Logo />
+        <Navbar language={this.props.language} />
+        <div className="content">
+          <Filter
+            selected={this.state.selectedSet}
+            active={this.state.activeSet}
+            changeFilter={this.handleFilterSelection}
+            previewFilter={this.handleFilterPreview}
+            language={this.props.language}
+          />
+          <Display
+            set={this.state.selectedSet}
+            selected={this.state.selectedProject}
+            active={this.state.activeProject}
+            changeProject={this.handleDisplaySelection}
+            previewProject={this.handleDisplayPreview}
+            language={this.props.language}
+          />
+        </div>
+        <ScrollBtn update={this.handleScrollBtn} style={this.state.style} />
+        <Watermark language={this.props.language} page="projects" />
+        <LinkBox />
+        <div className="mobile-footer" />
+      </div>
+    )
+  }
 }
 
 export default Projects
--- app/components/about/AboutRight.js
+++ app/components/about/AboutRight.js
@@ -12,21 +12,19 @@ const AboutRight = props => {
                 </h2>
                 <p className="about-text">
                     {props.language === "pt-BR"
-                        ? `Como desenvolvedor, essa é a minha filosofia: ter um site com design bem-feito, usável, intuitivo de se navegar - e agradável - é tão importante quanto ter um site funcional.`
-                        : `As a developer, that's my core philosophy: having your site
-                    be well-designed, usable, intuitive to navigate - and feel
-                    good - is just as important as it working properly.`}
+                        ? `Como desenvolvedor, essa é a minha filosofia: ter um site com design bem-feito, usável, intuitivo de se navegar, que funciona como esperado - e é agradável - são partes igualmente importantes da experiência do usuário.`
+                        : `As a developer, that's my core philosophy: having your site be well-designed, usable, intuitive to navigate, work properly - and feel good - are all equally important parts of the user's experience.`}
                 </p>
                 <p className="about-text">
                     {props.language === "pt-BR"
-                        ? `Eu sou um web designer e desenvolvedor front-end de 21 anos morando em São Paulo, Brasil. Além de design e programação, eu amo gatos, pássaros, crianças, videogames com histórias interessantes e moda. Além disso, sou um grande fã de design bicolor de alto contraste - preto e branco em particular.`
-                        : `I'm a 21-year-old web designer and front-end web developer based in São Paulo, Brasil. Besides design and coding, I love cats, birds, kids, story-driven video games and fashion. Also, I'm a big fan of high-contrast, well-balanced two-color design - black and white design in particular.` /*I have ADHD, and my friends say that most of the time my behavior is between that of a cat and a kid - but I'm always hyperfocused when designing or coding.*/}
+                        ? `Eu sou um web designer e desenvolvedor front-end de 21 anos morando em São Paulo, Brasil, que se sente confortável trabalhando com CSS, SASS, HTML,
+						JavaScript e React.js. Tenho um conhecimento básico de UX/UI, sou versado em Node.js, usuário prolífico de Git e do Github, e também sei trabalhar com D3.js.`
+                        : `I'm a 21-year-old front-end web developer based in São Paulo, Brazil, who feels most comfortable working with CSS, SASS, HTML, JavaScript and React.js. I have some knowledge of UX/UI, am well-versed in Node.js, a prolific user of Git and Github, and I can work with D3.js, too.` /*I have ADHD, and my friends say that most of the time my behavior is between that of a cat and a kid - but I'm always hyperfocused when designing or coding.*/}
                 </p>
                 <p className="about-text">
                     {props.language === "pt-BR"
-                        ? `Eu tenho confiança na minha habilidade para trabalhar com CSS, SASS, HTML,
-						JavaScript e React.js. Além disso tenho um conhecimento básico de UX/UI, sou versado em Node.js, usuário prolífico de Git e do Github, e também sei trabalhar com D3.js.`
-                        : `I should say that I have confidence in my CSS, SASS, HTML, JavaScript and React.js skills. I also have some basic knowledge of UX/UI, am well-versed in Node.js, a prolific user of Git and Github, and I can work with D3.js, too.`}
+                        ? `Além de design e programação, eu amo gatos, pássaros, crianças, videogames com histórias interessantes e moda. Além disso, sou um grande fã de design bicolor de alto contraste - preto e branco em particular.`
+                        : `Besides design and coding, I love cats, birds, kids, story-driven video games and fashion. Also, I'm a big fan of high-contrast, well-balanced two-colour design - black and white design in particular.`}
                 </p>
             </article>
         </div>
